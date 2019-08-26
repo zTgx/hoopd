693f712eaf1870eceb7c4c53b4523e27e2f8fe94
zTgx <747674262@qq.com>
2019-08-27 00:06:11 +0800

    add cpp-httplib

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <httplib.h>
+
+using namespace httplib;
 
 int main() {
-    std::cout << "Hello world." << std::endl;
+  Server svr;
+
+  svr.Get("/", [](const Request& req, Response& res) {
+      res.set_content("Hello World!", "text/plain");
+  });
+
+  svr.Get("/hi", [](const Request& req, Response& res) {
+      res.set_content("Hello World!", "text/plain");
+
+  });
+
+  svr.Get(R"(/numbers/(\d+))", [&](const Request& req, Response& res) {
+      auto numbers = req.matches[1];
+      res.set_content(numbers, "text/plain");
+  });
+
+  svr.Get("/stop", [&](const Request& req, Response& res) {
+      svr.stop();
+  });
+
+  svr.listen("localhost", 1234);
 }
