f182b60de1fbe59e104c904319bd5c398056a976
zTgx <747674262@qq.com>
2019-08-27 23:13:07 +0800

    add log examples

--- src/main.cc
+++ src/main.cc
@@ -67,6 +67,10 @@ void run() {
 
 int main() {
   spdlog::info("Welcome to spdlog!");
+  spdlog::error("Some error message with arg: {}", 1);
+  spdlog::warn("Easy padding in numbers like {:08d}", 12);
+  spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+  spdlog::debug("This message should be displayed..");
 
   init_spt();
   run();
