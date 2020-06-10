14ffbb97ea079b5b6b0dff69a9f5623a383acdaa
zTgx <747674262@qq.com>
2020-06-10 18:54:09 +0800

    17 => 11

--- src/main.cc
+++ src/main.cc
@@ -34,12 +34,8 @@ void run(std::string& s) {
 }
 
 int main() {
-  spdlog::info("Welcome to spdlog!");
-  spdlog::error("Some error message with arg: {}", 1);
-  spdlog::warn("Easy padding in numbers like {:08d}", 12);
-  spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-  spdlog::debug("This message should be displayed..");
+    std::string welcome("Welcome to hoopd!");
+    spdlog::info(welcome);
 
-  // std::string s = init_spt();
-  // run(s);
+    run(welcome);
 }
