3f314fa0881a11646d5acad2a95e2e62635f7bd3
zTgx <747674262@qq.com>
2021-07-15 20:13:14 +0800

    refactor: rename 3rd -> thirdparty

--- src/internal/parser.h
+++ src/internal/parser.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include <unordered_map>
 #include <internal/nocopyable.h>
-#include <3rd/httpparser/http_parser.h>
+#include <thirdparty/httpparser/http_parser.h>
 
 #include <hoopd.h>
 
